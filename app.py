9d666c1af2bef436bbf1e9a417810bf43deac81d
SwathiBabu01 <sbabu4@depaul.edu>
2022-11-14 01:48:58 -0600

    change in code

--- app.py
+++ app.py
@@ -349,11 +349,29 @@ if option == 'New User':
             get_book_info(b, books)
         users.loc[users['User-ID']==int(userID),'prevRec'] = bkStr
         edit_csv(bkStr, int(userID))
-        repo = Repo('.')  
+        '''repo = Repo('.')  
         repo.index.add(['data/Ratings.csv'])
         repo.index.commit('my commit description')
         origin = repo.remote('origin')
-        origin.push()
+        origin.push()'''
+        empty_repo = Repo.init(os.path.join(rw_dir, "empty"))
+        origin = empty_repo.create_remote("origin", Repo.remotes.origin.url)
+        assert origin.exists()
+        assert origin == empty_repo.remotes.origin == empty_repo.remotes["origin"]
+        origin.fetch()  # assure we actually have data. fetch() returns useful information
+        # Setup a local tracking branch of a remote branch
+        empty_repo.create_head("main", origin.refs.master)  # create local branch "master" from remote "master"
+        empty_repo.heads.master.set_tracking_branch(origin.refs.master)  # set local "master" to track remote "master
+        empty_repo.heads.master.checkout()  # checkout local "master" to working tree
+        # Three above commands in one:
+        empty_repo.create_head("master", origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+        # rename remotes
+        origin.rename("new_origin")
+        # push and pull behaves similarly to `git push|pull`
+        origin.pull()
+        origin.push()  # attempt push, ignore errors
+        origin.push().raise_if_error()  # push and raise error if it fails
+# assert not empty_repo.delete_remote(origin).exists()     # create and delete remotes
         #ratings_pivot.to_csv('data/Ratings_Pivot.csv')
 
 #PROCESS FOR EXISTING USER
