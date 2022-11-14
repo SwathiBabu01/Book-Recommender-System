74552d15f06905167ceb2197a1324ecc69d03310
SwathiBabu01 <sbabu4@depaul.edu>
2022-11-14 01:15:28 -0600

    change in code

--- app.py
+++ app.py
@@ -350,6 +350,7 @@ if option == 'New User':
         users.loc[users['User-ID']==int(userID),'prevRec'] = bkStr
         edit_csv(bkStr, int(userID))
         repo = Repo('.')  
+        repo.index.add(['Ratings.csv'])
         repo.index.commit('my commit description')
         origin = repo.remote('origin')
         origin.push()
